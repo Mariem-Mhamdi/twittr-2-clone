d6ba4f52a5462d410b62174ce336a7af3e2e5599
Maryem <mhamdimariem14@gmail.com>
2022-06-06 12:21:13 +0200

    app build

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZnPLPzVXEn4a9xfKA7UqJz1PrZAaGhAY",
+    authDomain: "twittr-2.firebaseapp.com",
+    projectId: "twittr-2",
+    storageBucket: "twittr-2.appspot.com",
+    messagingSenderId: "160562606008",
+    appId: "1:160562606008:web:956cdd2490361e399fa94d"
+};
